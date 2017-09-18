c9447950dcf5882b0d1062a988f4a90e76167c7e
wyang <wangyang@boyuntong.com>
2017-09-18 18:16:03 +0800

    'add markdown'

---
+++ backend/views/learning/view.php
@@ -0,0 +1,46 @@
+<?php
+
+use yii\helpers\Html;
+use yii\helpers\Markdown;
+use yii\widgets\DetailView;
+
+$this->title = $model->title;
+
+?>
+<div class="learning-view">
+
+    <?= DetailView::widget([
+        'model' => $model,
+        'template' => '<tr><th class="view_header" {captionOptions}>{label}</th><td{contentOptions}>{value}</td></tr>',
+        'attributes' => [
+            'id',
+            'title',
+            'author',
+            'category_format',
+            [
+                'attribute' => 'cover',
+                'format' => 'image',
+                'contentOptions' => ['class' => 'view_cover'],
+            ],
+            [
+                'attribute' => 'content',
+                'value' => function($model) {
+                    return Markdown::process($model->content, 'gfm');
+                },
+                'format' => 'raw',
+                'contentOptions' => ['class' => 'view_content'],
+            ],
+            'remend_format',
+            'status_format',
+            [
+                'attribute' => 'created_at',
+                'format' => ['date', 'php:Y-m-d H:i:s'],
+            ],
+            [
+                'attribute' => 'updated_at',
+                'format' => ['date', 'php:Y-m-d H:i:s'],
+            ],
+        ],
+    ]) ?>
+
+</div>
